5d2f3ed19d1b5e5446e436aec5088be964072e75
Thinh Nguyen Dang <dangthinhvh@gmail.com>
2021-09-29 16:15:13 +0700

    Add Home page in frontend

---
+++ frontend/src/components/VTag.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link :to="homeRoute" :class="className" v-text="name"></router-link>
+</template>
+
+<script>
+export default {
+  name: "KrvTag",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    className: {
+      type: String,
+      default: "tag-pill tag-default",
+    },
+  },
+  computed: {
+    homeRoute: () => ({ name: "home-tag", params: { tag: name } }),
+  },
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">Kite Runner</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="feed-toggle">
+            <ul class="nav nav-pills outline-active">
+              <li v-if="isAuthenticated" class="nav-item">
+                <router-link
+                  :to="{ name: 'home-my-feed' }"
+                  class="nav-link"
+                  active-class="active"
+                >
+                  Your Feed
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link
+                  :to="{ name: 'home' }"
+                  exact
+                  class="nav-link"
+                  active-class="active"
+                >
+                  Global Feed
+                </router-link>
+              </li>
+              <li class="nav-item" v-if="tag">
+                <router-link
+                  :to="{ name: 'home-tag', params: { tag } }"
+                  class="nav-link"
+                  active-class="active"
+                >
+                  <i class="ion-pound"></i> {{ tag }}
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          <router-view></router-view>
+        </div>
+        <div class="col-md-3">
+          <div class="sidebar">
+            <p>Popular Tags</p>
+            <div class="tag-list">
+              <KrvTag v-for="(tag, index) in tags" :name="tag" :key="index">
+              </KrvTag>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { mapGetters } from "vuex";
+import KrvTag from "@/components/VTag";
+import { FETCH_TAGS } from "@/store/actions.type";
 export default {
-  name: "Home",
+  name: "home",
   components: {
-    HelloWorld,
+    KrvTag,
+  },
+  mounted() {
+    this.$store.dispatch(FETCH_TAGS);
+  },
+  computed: {
+    ...mapGetters(["isAuthenticated", "tags"]),
+    tag() {
+      return this.$route.params.tag;
+    },
   },
 };
 </script>
